498d921e6e88cea87cc99cf7f2e7f9e02e9b31aa
Kosei Oki <koseioki1225@gmail.com>
2026-01-04 19:48:55 +0100

    added first svgs

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,12 +1,17 @@
 import { NavLink } from "react-router-dom";
 import "./Home.css";
+
+import {Icon} from "../Components/Global/Icon";
+import {Illustration} from "../Components/Global/Illustration";
+
+
 export function Home() {
   return (
     <main>
       <h1>How can we make the digital world inclusive?</h1>
       <p>
         At Intersectional Design Lab, we research and share design practices for
-        Information and Communication Technologies (ICT) to make the digital
+        Information and Communication Technologies (ICT) from an <i>intersectional perspective</i> to make the digital
         space a more inclusive place for everyone.
       </p>
       <NavLink to="/what-is-intersectional-design">
@@ -15,24 +20,34 @@ export function Home() {
       <h2>What We Offer</h2>
       <ul className="offer-list">
         <li>
-          <h3><NavLink to="/">Design in Practice Guide</NavLink></h3>
+          <article>
+          <h3><NavLink to="/">Design in Practice Guide<Icon /></NavLink></h3>
           <p>
             We share practical design guides for each design element from an
             intersectional perspective.
           </p>
+          <Illustration />
+          </article>
         </li>
         <li>
-          <h3><NavLink to="/">Seminars & Workshops</NavLink></h3>
+          <article>
+          <h3><NavLink to="/">Seminars & Workshops<Icon /></NavLink></h3>
+
           <p>
             We host seminars, webinars, workshops, talks, and similar events.
           </p>
+          <Illustration />
+          </article>
         </li>
         <li>
-          <h3><NavLink to="/">Resource Library</NavLink></h3>
+          <article>
+          <h3><NavLink to="/">Resource Library<Icon /></NavLink></h3>
           <p>
             We share our curated research resources, including books, videos,
             courses, and more.
           </p>
+          <Illustration />
+          </article>
         </li>
       </ul>
     </main>
