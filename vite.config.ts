9d91b04779c4700103e2d775bf6f4e86c22b8945
Kosei Oki <koseioki1225@gmail.com>
2026-01-09 18:51:41 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
+// export default defineConfig(({ command }) => {
+    export default defineConfig(() => {
+
     const config = {
         plugins: [react()],
         base: "/"
     };
 
-    if (command !== "serve") {
-        config.base = "/idl/";
-    }
+    // if (command !== "serve") {
+    //     config.base = "/idl/";
+    // }
 
     return config;
 });
