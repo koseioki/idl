b0d458f9b9638e12253ef84ed9e47f7abe5f9e06
Kosei Oki <koseioki1225@gmail.com>
2026-02-19 19:21:17 +0100

    Update ResearchProjects.tsx

--- src/Pages/ResearchProjects.tsx
+++ src/Pages/ResearchProjects.tsx
@@ -1,3 +1,4 @@
+import { Card } from "../Components/Global/Card";
 export function ResearchProjects() {
   return (
     <>
@@ -6,6 +7,33 @@ export function ResearchProjects() {
       </head>
       <main id="main-content">
         <h1>Research Projects</h1>
+                <p>Coming soon! This page is under construction.</p>
+  <ul className="card-list">
+        <li>
+          <Card
+          heading="Marginalisation and Design"
+          headingLevel={2}
+          linkTo="/research-projects/marginalisation-and-design"
+          disabled={false}>
+            <p>
+              What kinds of marginalisation exist, and how do they affect design?
+            </p>
+            {/* <Drawing /> */}
+          </Card>
+        </li>
+        <li>
+          <Card
+          heading="Design in Practice"
+          headingLevel={2}
+          linkTo="/research-projects/design-in-practice"
+          disabled={false}>
+            <p>
+              We discuss how design can become more inclusive, with practical examples.
+            </p>
+            {/* <Drawing /> */}
+          </Card>
+        </li>
+</ul>
       </main>
     </>
   );
