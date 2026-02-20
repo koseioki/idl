2b172b7ceade3bfa3a3341a54dc58c6d24c38e74
Kosei Oki <koseioki1225@gmail.com>
2026-02-20 20:30:05 +0100

    typo

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -11,9 +11,9 @@ export function Home() {
   return (
     <>
       <main id="main-content">
-        <h1>How can we make the digital world inclusive?</h1>
+        <h1>How can we make the digital world more inclusive?</h1>
         <p>
-          At Intersectional Design Lab, wwe research, experiment with, and
+          At Intersectional Design Lab, we research, experiment with, and
           sharedesign practices for Information and Communication Technologies
           (ICT) from an <i>intersectional perspective</i> to make the digital
           space a more inclusive place for everyone.
