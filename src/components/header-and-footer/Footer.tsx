0e33c642159df4b3babc70f2c7d0358ac4d8cf6c
Kosei Oki <koseioki1225@gmail.com>
2026-09-12 18:01:29 +0200

    updated tel

--- src/components/header-and-footer/Footer.tsx
+++ src/components/header-and-footer/Footer.tsx
@@ -12,7 +12,7 @@ export function Footer() {
           </a>
         </li>
         <li>
-          <a href="tel:+4591964325">+45 91 96 43 25</a>
+          <a href="tel:+46 72-770 23 33">+46 72-770 23 33</a>
         </li>
         <li>
           <a href="https://www.linkedin.com/company/intersectional-design-lab/">
